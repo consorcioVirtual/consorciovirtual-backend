d120ca62fe05b3933235c97e79696e9bf7bf0d14
santijas <santi.ranieri@gmail.com>
2021-06-03 20:33:49 -0300

    First commit app.

---
+++ src/main/java/ar/edu/unsam/consorciovirtual/ConsorcioVirtualApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unsam.consorciovirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsorcioVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsorcioVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unsam/consorciovirtual/ConsorcioVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unsam.consorciovirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsorcioVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
