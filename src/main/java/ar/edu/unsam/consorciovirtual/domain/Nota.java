39ad14e69c9a9ac3fd48c7e9ae75f4f32716d2bd
santil99 <santiilorenzo9499@gmail.com>
2021-07-15 00:36:29 -0300

    Se agrega busqueda por tipo de solicitud tecnica en service

--- src/main/java/ar/edu/unsam/consorciovirtual/domain/Nota.java
+++ src/main/java/ar/edu/unsam/consorciovirtual/domain/Nota.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static ar.edu.unsam.consorciovirtual.domain.Constants.ZONE_ID_ARGENTINA;
