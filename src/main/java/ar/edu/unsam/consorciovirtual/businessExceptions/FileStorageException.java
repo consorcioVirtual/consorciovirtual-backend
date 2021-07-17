9eaacda921c17267f8dd9f235886a68ac8250cae
santijas <santi.ranieri@gmail.com>
2021-07-17 00:18:21 -0300

    File uploader y otros

---
+++ src/main/java/ar/edu/unsam/consorciovirtual/businessExceptions/FileStorageException.java
@@ -0,0 +1,11 @@
+package ar.edu.unsam.consorciovirtual.businessExceptions;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
