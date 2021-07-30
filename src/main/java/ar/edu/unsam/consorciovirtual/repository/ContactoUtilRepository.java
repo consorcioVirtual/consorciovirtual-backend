3eacebf7429f9a56ade97e4a0aa31b46dc5df0a1
Naahueeh <nahuelramos518@gmail.com>
2021-07-29 23:56:35 -0300

    cambio en busqueda de teléfonos útiles

--- src/main/java/ar/edu/unsam/consorciovirtual/repository/ContactoUtilRepository.java
+++ src/main/java/ar/edu/unsam/consorciovirtual/repository/ContactoUtilRepository.java
@@ -2,6 +2,8 @@ package ar.edu.unsam.consorciovirtual.repository;
 
 import ar.edu.unsam.consorciovirtual.domain.ContactoUtil;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +11,7 @@ public interface ContactoUtilRepository extends JpaRepository<ContactoUtil, Long
 
     List<ContactoUtil> findAllByBajaLogicaFalseAndServicioContaining(String palabraBuscada);
 
+    @Query(value = "SELECT contacto FROM contacto_util as contacto WHERE contacto.servicio LIKE %:palabraBuscada% " +
+            "AND contacto.baja_logica = false;", nativeQuery = true)
+    List<ContactoUtil> buscarTodos(@Param("palabraBuscada") String palabraBuscada);
 }
